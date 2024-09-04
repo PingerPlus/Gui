2a7f2ddedf26d9cd4aec10d0672f31ff4c0ad3e5
Dimitrije Mijailovic <pinger.business@gmail.com>
2024-09-04 22:23:04 +0200

    📝 Documentation for Iterables#query

--- src/main/java/io/pnger/gui/util/Iterables.java
+++ src/main/java/io/pnger/gui/util/Iterables.java
@@ -7,6 +7,21 @@ import javax.annotation.Nonnull;
 
 public interface Iterables {
 
+    /**
+     * Filters the given iterable based on the result of applying the specified function to each element
+     * and comparing it to the given parameter.
+     *
+     * <p>For each element in the provided iterable, the function is applied. If the result of the function
+     * is not {@code null} and equals the specified parameter, the element is added to the resulting list.
+     *
+     * @param iterable the iterable containing the elements to filter
+     * @param function the function to apply to each element of the iterable
+     * @param param the parameter to compare the result of the function against, must not be null
+     * @param <R> the type of elements in the iterable
+     * @param <T> the type of the parameter to compare the function's result against
+     * @return a list containing all elements from the iterable for which the function result matches the specified parameter
+     */
+
     static <R, T> List<R> query(Iterable<R> iterable, Function<R, T> function, @Nonnull T param) {
         final List<R> list = new ArrayList<>();
         for (final R r : iterable) {
